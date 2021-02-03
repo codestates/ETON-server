1b72667f95b1b8b5b9f92043b018616ad67db9cb
aesop <ee.changsub@gmail.com>
2021-02-03 20:01:32 +0900

    Complete Logout.js
    - accesstoken 삭제 역할
    - refreshToken 삭제 역할

--- controllers/users/Logout.js
+++ controllers/users/Logout.js
@@ -1,17 +1,17 @@
 const { users } = require('../../models');
 
 module.exports = (req, res) => {
-  console.log('=====req.headers======')
-  console.log(req.headers)
-  console.log('=====================')
-  console.log('======req.headers.accessToken===========')
-  console.log(req.headers.accessToken)
-  console.log('=============================================')
-
-  if (!req.cookies.refreshToken)
-    res.status(400).json({data: null, message: 'not authorized'});
-  else {
+  // console.log('=====req.headers======')
+  // console.log(req.headers)
+  // console.log('=====================')
+  // console.log("======req.headers['accesstoken']===========")
+  // console.log(req.headers['accesstoken'])
+  // console.log('=============================================')
+  if(req.headers['accesstoken'] && req.cookies.refreshToken) {
+    delete req.headers['accesstoken']
     res.clearCookie('refreshToken');
-    res.json({ data: null, message: 'ok' });
+    res.json({ data: null, message: "You're logged out." });
+  } else {
+    res.status(404).json({data:null, message: "Not authorized."})
   }
 }
