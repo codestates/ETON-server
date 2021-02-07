b0f64723a323b54abca913eb173c02da3f5f6e1a
aesop <ee.changsub@gmail.com>
2021-02-07 20:35:48 +0900

    Add sequelizer basic configuration
    config/config.json -> config/config.js
    babel-register devDependencies에 추가

---
+++ config/config.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: "localhost",
+    dialect: "mysql",
+  },
+  test: {
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    host: "localhost",
+    dialect: "mysql",
+  },
+  production: {
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_NAME,
+    host: process.env.RDS_HOST,
+    port: process.env.RDS_PORT,
+    dialect: "mysql",
+  },
+};
