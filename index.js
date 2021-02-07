295540b07eec50bae0e879badd6b9388b02fc8c6
aesop <ee.changsub@gmail.com>
2021-02-07 22:17:46 +0900

    Edit https-related part

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 const fs = require("fs");
-const https = require("https");
-const cors = require("cors");
+// const https = require("https");
+// const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const privateKey = fs.readFileSync(process.env.KEY_PATH, "utf8");
-const certificate = fs.readFileSync(process.env.CERT_PATH, "utf8");
-const credentials = { key: privateKey, cert: certificate };
+// const privateKey = fs.readFileSync(process.env.KEY_PATH, "utf8");
+// const certificate = fs.readFileSync(process.env.CERT_PATH, "utf8");
+// const credentials = { key: privateKey, cert: certificate };
 const usersRouter = require("./routes/users");
 const boardRouter = require("./routes/boards");
 
@@ -14,12 +14,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(
-  cors({
-    origin: ["https://localhost:3000"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: ["https://localhost:3000"],
+//     credentials: true,
+//   })
+// );
 
 app.get("/", (req, res) => {
   res.json("hey");
@@ -29,10 +29,8 @@ app.use(cookieParser());
 app.use("/users", usersRouter);
 app.use("/boards", boardRouter);
 
-const HTTPS_PORT = process.env.HTTPS_PORT;
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(HTTPS_PORT, () =>
-  console.log(`server runnning on ${HTTPS_PORT}`)
-);
+// const HTTPS_PORT = process.env.HTTPS_PORT;
+// const httpsServer = https.createServer(credentials, app);
+app.listen(4000, () => console.log(`server runnning on 4000`));
 
 module.exports = httpsServer;
