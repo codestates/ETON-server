59bda9adfb87b5914f94b0422ed1e2ffccaf7163
aesop <ee.changsub@gmail.com>
2021-02-08 14:01:09 +0900

    Restore cors origin option

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: true, //! cors warning 발생 시 필요
+    origin: "*", //! cors warning 발생 시 필요
     credentials: true,
   })
 );
