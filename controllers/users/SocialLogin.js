5e762bcf14f237235b279413a2a6946b7208abf1
aesop <ee.changsub@gmail.com>
2021-02-09 15:53:40 +0900

    Remove refresh token-related code

--- controllers/users/SocialLogin.js
+++ controllers/users/SocialLogin.js
@@ -1,55 +1,57 @@
-const { users } = require('../../models');
+const { users } = require("../../models");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const client_id = process.env.GITHUB_CLIENT_ID;
 const client_secret = process.env.GITHUB_CLIENT_SECRET;
-const axios = require('axios');
-const { 
+const axios = require("axios");
+const {
   generateAccessToken,
   generateRefreshToken,
   sendAccessToken,
-  sendRefreshToken
-} = require('../tokenFunctions');
+  sendRefreshToken,
+} = require("../tokenFunctions");
 
 async function callback(req, res, next) {
   try {
     console.log(req.body);
-  
-    const githubToken = await axios.post('https://github.com/login/oauth/access_token', {
+
+    const githubToken = await axios.post(
+      "https://github.com/login/oauth/access_token",
+      {
         client_id,
         client_secret,
-        code: req.body.authorizationCode
-      }, {
+        code: req.body.authorizationCode,
+      },
+      {
         headers: {
-          accept: 'application/json'
+          accept: "application/json",
         },
       }
-    )
+    );
 
     const githubData = await axios.get("https://api.github.com/user", {
-      headers : {
-          "authorization" : `token ${githubToken.data.access_token}`,
-          "accept" : "application/json"
-        }
-      }
-    );
-    console.log(githubData.data)
+      headers: {
+        authorization: `token ${githubToken.data.access_token}`,
+        accept: "application/json",
+      },
+    });
+    console.log(githubData.data);
     const { email } = githubData.data.user;
-    const exUser = await users.findOne( { where: { email }} );
+    const exUser = await users.findOne({ where: { email } });
     if (!exUser) {
       await users.create({
-        email
-      })
+        email,
+      });
     }
-      const accessToken = generateAccessToken(exUser.dataValues)
-      const refreshToken = generateRefreshToken(data.dataValues)
+    const accessToken = generateAccessToken(exUser.dataValues);
+    // const refreshToken = generateRefreshToken(data.dataValues)
 
-      sendRefreshToken(res, refreshToken);
-      sendAccessToken(res, accessToken);
-  } catch(e) {
+    // sendRefreshToken(res, refreshToken);
+    sendAccessToken(res, accessToken);
+  } catch (e) {
     next(err);
   }
 }
 
-module.exports = {callback};
+module.exports = { callback };
