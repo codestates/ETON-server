c723aa28193692230f18cdf52ebc8680c1b01c8a
aesop <ee.changsub@gmail.com>
2021-02-11 23:05:59 +0900

    hotfix/feature/createNewTask

--- controllers/task/postTask.js
+++ controllers/task/postTask.js
@@ -1,54 +1,58 @@
-const {tasks, progresses} = require("../../models");
+const { tasks, progresses } = require("../../models");
 
 module.exports = {
-    createTask : async (req, res) => {
-        console.log("Create Task = req.body : ", req.body);
+  createTask: async (req, res) => {
+    console.log("Create Task = req.body : ", req.body);
 
-        if (req.headers["authorization"]) {
-
-            await tasks.create({
-                title : req.body.title,
-                progress_id : Number(req.body.progress_id)
+    if (req.headers["authorization"]) {
+      await tasks
+        .create({
+          title: req.body.title,
+          progress_id: Number(req.body.progress_id),
+        })
+        .then((result) => {
+          console.log("@@@", result);
+          let newTaskId = result.dataValues.id;
+          progresses
+            .findOne({
+              attributes: ["id", "title", "task_priority"],
+              where: { id: result.dataValues.progress_id },
             })
-            .then(result => {
-                console.log("@@@",result);
-                let newTaskId = result.dataValues.id;
-                progresses.findOne({
-                    attributes : ['id', 'title', 'task_priority'],
-                    where : {id : result.dataValues.progress_id}
-                })
-                .then(result => {
-                    console.log("###",result);
-                    console.log("newTaskId : ", newTaskId);
-                    let newOrder = String(newTaskId);
-                    if(result.task_priority !== ''){
-                        newOrder = ","+newOrder
-                    }
-                    progresses.update({
-                        task_priority : result.task_priority + newOrder
-                        // task_priority : [...result.task_priority, String(newTaskId)] 
-                    },{
-                        where : {id : result.dataValues.id}
-                    })
-                    .then(result => {
-                        console.log("성공!");
-                        console.log(result);
-                        res.status(200).send({message : "ok"});
-                    })
-                })
-                // progresses.update({
-                //     task_priority : 
-                // },{
-                //     whrere : {id : result.dataValues.progress_id}
-                // })
-            })
-            .catch(err => {
-                console.log("err : ", err);
-                res.send(404).send({message : 'create error'})
-            })
-
-        }else{
-            res.status(403).send({message : "Invalid access token."})
-        }
+            .then((result) => {
+              console.log("###", result);
+              console.log("newTaskId : ", newTaskId);
+              let newOrder = String(newTaskId);
+              if (result.task_priority !== "") {
+                newOrder = "," + newOrder;
+              }
+              progresses
+                .update(
+                  {
+                    task_priority: result.task_priority + newOrder,
+                    // task_priority : [...result.task_priority, String(newTaskId)]
+                  },
+                  {
+                    where: { id: result.dataValues.id },
+                  }
+                )
+                .then((result) => {
+                  console.log("성공!");
+                  console.log(result);
+                  res.status(200).send({ id: newTaskId, message: "ok" });
+                });
+            });
+          // progresses.update({
+          //     task_priority :
+          // },{
+          //     whrere : {id : result.dataValues.progress_id}
+          // })
+        })
+        .catch((err) => {
+          console.log("err : ", err);
+          res.send(404).send({ message: "create error" });
+        });
+    } else {
+      res.status(403).send({ message: "Invalid access token." });
     }
-}
+  },
+};
